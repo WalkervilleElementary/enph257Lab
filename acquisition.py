d68d563a6be31b8eaa3d9b496f5c10b2a4b1a78e
fb39ca4 <fb39ca4@gmail.com>
2017-06-05 15:03:17 -0700

    Heating control

--- acquisition.py
+++ acquisition.py
@@ -4,6 +4,7 @@ import time
 from datetime import datetime
 from math import ceil
 
+period = 10 * 60 #seconds
 filename = str(datetime.utcnow()).split(".")[0].replace(" ", "_") + ".csv"
 out_file = open(filename, "w")
 
@@ -15,7 +16,8 @@ with serial.Serial('/dev/ttyACM0', 9600, timeout=1) as ser:
     get_time.start_time = time.monotonic()
     while True:
         sample_time = get_time()
-        ser.write(bytes(b'\x00'))
+        heat = True if (sample_time / period) % 1 < 0.5 else False
+        ser.write(bytes(b'H' if heat else b'L'))
         raw = ser.readline()
         entries = ["{0:.3f}".format(sample_time)]
         try:
@@ -26,6 +28,7 @@ with serial.Serial('/dev/ttyACM0', 9600, timeout=1) as ser:
         except:
             entries.append('"bad sample"')
             entries.append(str(raw))
+        entries.append(1 if heat else 0)
         #print(entries)
         print("\t".join(entries))
         out_file.write(",".join(entries) + "\n")
